ae371a29f9004a496c40daab0141686175cd5efe
ranpariyachirag <25187983+ranpariyachirag@users.noreply.github.com>
2023-05-09 09:58:33 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 // import Hero from './Hero';
-// import Navbar from './Navbar';
+import Navbar from './Navbar';
 // import Card from './Card';
 // import data from './data';
+
+export default function App() {
+  return <Navbar />;
+}
