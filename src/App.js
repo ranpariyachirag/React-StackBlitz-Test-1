f168493925828395ce6b8b972acc0d7b653e1128
ranpariyachirag <25187983+ranpariyachirag@users.noreply.github.com>
2023-05-09 09:53:40 -0400

    5 files modified

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React from 'react';
 
-import Hero from './Hero';
-import Navbar from './Navbar';
-import Card from './Card';
-import data from './data';
-
-export default function App() {
-  const cards = data.map((item) => {
-    return <Card key={item.id} {...item} />;
-  });
-
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <section className="cards-list">{cards}</section>
-    </div>
-  );
-}
+// import Hero from './Hero';
+// import Navbar from './Navbar';
+// import Card from './Card';
+// import data from './data';
