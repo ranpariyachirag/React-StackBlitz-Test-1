a86943d2975031d16a5017da96941b4bde56a1c6
ranpariyachirag <25187983+ranpariyachirag@users.noreply.github.com>
2023-05-10 16:38:03 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Hero from './Hero';
 import Navbar from './Navbar';
-// import Card from './Card';
+import Card from './Card';
 import data from './data';
 
 export default function App() {
   const cards = data.map((item) => {
-    return <Card key={item.id} title={item.title} />;
+    return <Card key={item.id} {...item} />;
   });
 
   return (
--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 export default function Card(props) {
+  let badge;
+
+  if (props.openSpots === 0) {
+    badge === 'sold out';
+  } else if (props.location === 'Online') {
+    badge = 'ONLINE';
+  }
+
   return (
     <div className="card">
+      {badge &&<div className="card--badge">{badge}</div>}
+      <img src={`../images/${props.coverImg}`} className="card--image" />
+      <div className="card--stats">
+        <img src="../images/star.png" className="card--star" />
+        <span>{props.stats.rating}</span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
+      </div>
       <p className="card--title">{props.title}</p>
+      <p className="card--price">
+        <span className="bold">From ${props.price}</span> / person
+      </p>
     </div>
   );
 }
